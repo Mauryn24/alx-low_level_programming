4ee52299e583977126fd8a7a6233b59434bf1807
Mauryn24 <maurynnyakio19@gmail.com>
2022-07-14 08:40:45 -0700

    buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,5 +17,8 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 void print_array(int *a, int n);
+void printASCII(char *b, int start, int end);
+void printHexes(char *b, int start, int end);
+int isPrintableASCII(int n);
 
 #endif
