f9b1b2c634d7ebbbf556c1ba030b311d2b1f204f
Mauryn24 <maurynnyakio19@gmail.com>
2022-07-22 05:57:07 -0700

    adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  adds positive numbers.
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0 but returns 1 if
+ * the string is a number
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
