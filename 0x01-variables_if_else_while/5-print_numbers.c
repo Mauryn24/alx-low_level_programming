01c26ac7e34aba868e0a5bce7d09b0c9da984503
Mauryn24 <maurynnyakio19@gmail.com>
2023-02-17 11:57:00 -0800

    prints numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
