c699805019f92b124acf61c61afb2e9c50e351b3
Mauryn24 <maurynnyakio19@gmail.com>
2023-02-23 11:31:36 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers between 0-100,
+ * finding their multiples
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+	}
+	printf('\n');
+	return (0);
+}
