bc8ab760dc73f2a932860b83ab21e440b2a5554c
Mauryn24 <maurynnyakio19@gmail.com>
2023-02-23 10:45:25 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return: 0 on success
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+		_putchar(c);
+	_putchar('\n');
+}
