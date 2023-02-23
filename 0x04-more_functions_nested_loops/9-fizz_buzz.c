fdd3a813f6313ee64fabad16171e222ffe2e59f4
Mauryn24 <maurynnyakio19@gmail.com>
2023-02-23 11:53:42 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,12 +17,10 @@ int main(void)
 			printf(" Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz");
-		else 
-			printf(" %d", i);
-		if (i == 100)
-			break;
+		else if (i == 1)
+			printf("%d", i);
 		else
-			printf(" ");
+			printf(" %d");
 	}
 	printf("\n");
 	return (0);
