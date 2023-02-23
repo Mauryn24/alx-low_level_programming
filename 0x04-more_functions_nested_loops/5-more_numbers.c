603490b9d1095388c942ef67bf95b708d0f506da
Mauryn24 <maurynnyakio19@gmail.com>
2023-02-23 10:57:13 -0800

    print numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers *10 times
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = '0'; b <= '14'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
