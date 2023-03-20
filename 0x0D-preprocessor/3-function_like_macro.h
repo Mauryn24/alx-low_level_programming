9a71d054066ced37f030dc064ba45504deaee173
Mauryn24 <maurynnyakio19@gmail.com>
2023-03-20 14:22:40 -0700

    absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? -1 * (x) : (x))
+/*if x is < 0, x is multiplied by -1 and value is returned*/
+/*else the value is returned*/
+
+#endif
