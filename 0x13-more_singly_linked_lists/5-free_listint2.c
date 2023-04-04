64dbd4a40a8d9f55610e20f3a55f42890ac7acc4
Mauryn24 <maurynnyakio19@gmail.com>
2023-04-04 14:59:29 -0700

    free nodes

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * and sets the head pointer to NULL
+ * @head: double pointer to the first element in the list
+ */
+void free_listint2(listint_t **head)
+{
+	/* Declare a pointer to the current node*/
+	listint_t *current;
+
+	/* Traverse the list, freeing each node as we go*/
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+
+	/*Set the head pointer to NULL*/
+	*head = NULL;
+}
