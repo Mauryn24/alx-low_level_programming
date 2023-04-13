4e189697f606fe4f424b25ee9cf66a71e8f8d1ad
Mauryn24 <maurynnyakio19@gmail.com>
2023-04-13 00:47:04 -0700

    read

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,6 +13,9 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
