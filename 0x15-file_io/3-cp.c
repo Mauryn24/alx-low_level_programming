12f4353afa100fa1e1205532a3b526197806aeda
Mauryn24 <maurynnyakio19@gmail.com>
2023-05-08 14:39:58 -0700

    copy a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,18 +9,21 @@
 
 int main(int argc, char *argv[])
 {
-	int fd1, fd2, chars_read, chars_written, file_closed, file2_closed;
+	int fd1, fd2, chars_read, chars_written, file_closed, f2_closed;
 	char buf[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "usage: cp file_from file-to\n", exit(97));
-	fd1 = open(arg[1], O_RDONLY, 0);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file-to\n");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY, 0);
 	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WROMLY, 0664);
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read to file %s\n", argv[2]);
@@ -46,10 +49,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
 		exit(100);
 	}
-	if (file2_closed != 0)
+	if (f2_closed != 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100)
+		exit(100);
 	}
 	return (0);
 }
